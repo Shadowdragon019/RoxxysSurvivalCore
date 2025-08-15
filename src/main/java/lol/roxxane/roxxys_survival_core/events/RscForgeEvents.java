5f7d5c718db11974cd7f8b54951b5a1ed0eaaeb0
Creature Girl <wolfboycoolkid@gmail.com>
2025-08-14 22:07:13 -0500

    Added tooltips for compost & burn time

--- src/main/java/lol/roxxane/roxxys_survival_core/events/RscForgeEvents.java
+++ src/main/java/lol/roxxane/roxxys_survival_core/events/RscForgeEvents.java
@@ -4,9 +4,14 @@ import lol.roxxane.roxxys_survival_core.Rsc;
 import lol.roxxane.roxxys_survival_core.commands.RscDevCommand;
 import lol.roxxane.roxxys_survival_core.configs.RscServerConfig;
 import lol.roxxane.roxxys_survival_core.data.RscDamageTypeIframesManager;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -30,5 +35,16 @@ public class RscForgeEvents {
 	@SubscribeEvent
 	public static void reload(AddReloadListenerEvent event) {
 		event.addListener(new RscDamageTypeIframesManager());
+	}
+	@SubscribeEvent
+	public static void tooltip(ItemTooltipEvent event) {
+		var stack = event.getItemStack();
+		var item = stack.getItem();
+		var burn_time = ForgeHooks.getBurnTime(stack, RecipeType.SMELTING);
+		var compost_chance = ComposterBlock.COMPOSTABLES.getFloat(item);
+		if (burn_time > 0)
+			event.getToolTip().add(Component.translatable("tooltip.burn_time_" + burn_time));
+		if (compost_chance > 0)
+			event.getToolTip().add(Component.translatable("tooltip.compost_chance_" + compost_chance));
 	}
 }
